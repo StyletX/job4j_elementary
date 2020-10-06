477c153c4bdeee33fdf3d782efb122c352cf128b
StyletX <styletx@gmail.com>
2020-10-06 12:46:13 +0400

    8. Стороны прямоугольника. [#382249] Расписал формулу на 3 попроще.

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,7 +2,9 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        double rsl = k * Math.pow(p / (2 * (k + 1)), 2);
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double rsl = h * l;
         return rsl;
     }
 
