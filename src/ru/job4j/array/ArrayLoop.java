9d8f92dd14679839cac7cfcb0ae8da78db7c65f5
StyletX <styletx@gmail.com>
2020-10-12 09:14:50 +0400

    6.0.3. Массивы и цикл for. [#382228] Поправил условие цикла.

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -3,13 +3,9 @@ package ru.job4j.array;
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] numbers = new int[5];
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i * 2 + 3;
         }
-
- /*       for (int i = 0; i < 5; i++) {
-            System.out.println(numbers[i]);
-        }*/
 
         for (int number : numbers) {
             System.out.println(number);
